c6a657f83ec1f3fe5513af59646f096303031ab0
Abdul Moqeet <amoqeet43@gmail.com>
2017-11-27 13:12:54 +0500

    record

--- src/com/musicrecord/shared/Records.java
+++ src/com/musicrecord/shared/Records.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Records implements Serializable {
-   // @idRecords
+   
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "idRecords")
     private int idRecords;
